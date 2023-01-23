9aa9ec15c65e039b536c4ab5bd337d69a9cf2760
Andrw21 <AHUGHE213@caledonian.ac.uk>
2023-01-23 06:03:49 +0000

    video push

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var podname= process.env.HOSTNAME; var startTime;
 var host;
 var handleRequest = function(request, response) { response.setHeader('Content-Type', 'text/plain'); response.writeHead(200);
 response.write("DevOps Coursework 2! | Running on: "); response.write(host);
-response.end(" | v=0\n");
+response.end(" | v=1\n");
 console.log("Running On:" ,host, "| Total Requests:", ++requests,"| App Uptime:", (new Date() - startTime)/1000 , "seconds", "| Log Time:",new Date());}
 var www = http.createServer(handleRequest); www.listen(8080,function () {
 startTime = new Date();;
